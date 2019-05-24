eb4f1026dcc364e88135c6ccc4ba4c58114d7cdc
nyamdava <i71_m@yahoo.com>
2019-05-24 11:59:50 +0800

    Initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Re-vents</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './app/layout/App';
+import * as serviceWorker from './serviceWorker';
+
+const rootEl = document.getElementById('root');
+
+let render = () => {
+    ReactDOM.render(<App />, rootEl)
+}
+
+
+if (module.hot) {
+    module.hot.accept('./app/layout/App', () => {
+        setTimeout(render)
+    })
+}
+
+render();
+serviceWorker.unregister();
+ 
